5051de8a038e7368ed1649d81ceca1739cdb743f
Andreacchio Michael <mandreacchio@atlassian.com>
2016-04-06 19:47:11 +1000

    Commiting, worts and all!

---
+++ src/net/andreacchio/algorithmicToolbox/week1/APlusB.java
@@ -0,0 +1,20 @@
+package net.andreacchio.algorithmicToolbox.week1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 18/03/16
+ * Time: 8:22 AM
+ * Purpose: Coursera Week1
+ */
+
+import java.util.Scanner;
+
+class APlusB {
+  public static void main(String[] args) {
+
+      Scanner s = new Scanner(System.in);
+      int a = s.nextInt();
+      int b = s.nextInt();
+      System.out.println(a + b);
+  }
+}
---
+++ src/net/andreacchio/algorithmicToolbox/week2/GCD.java
@@ -0,0 +1,25 @@
+package net.andreacchio.algorithmicToolbox.week2;
+
+import java.util.Scanner;
+
+public class GCD {
+
+
+    private static int gcd(int a, int b) {
+
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(gcd(a, b));
+    }
+
+}
